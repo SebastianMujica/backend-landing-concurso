0dd5e860ea410364a93f8aa821ffef8c53b564a7
sebastianmujica@gmail.com <sebastianmujica@gmail.com>
2023-10-04 17:57:36 +0200

    fix

--- routes/cuponRoutes.js
+++ routes/cuponRoutes.js
@@ -5,6 +5,6 @@ module.exports = (app) => {
 
   //app.post('/api/cupones/register', cuponesController.register);
   app.get('/api/cupones/getAllForUser', passport.authenticate('jwt',{session: false}) , cuponesController.getAllForId);
-  app.post('/api/cupones/create', passport.authenticate('jwt',{session: false}) , cuponesController.register);
-
+  //app.post('/api/cupones/create', passport.authenticate('jwt',{session: false}) , cuponesController.register);
+  app.post('/api/cupones/create', cuponesController.register);
 }
